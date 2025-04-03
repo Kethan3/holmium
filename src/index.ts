ade5fd2e9d66b25118cb3888bcd0cf1721667258
Kethan3 <kethangowda003@gmail.com>
2025-04-03 11:38:48 +0530

    initialize project with Docker setup, Node.js configuration, and basic API endpoints

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/ping", (c) => {
+  return c.json({
+    pong: "Hello, World",
+  });
+});
+
+app.get("/platform", (c) => {
+  return c.json({
+    platform: process.platform,
+  });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
